fd698924508a9fa274cf25b51ee1f8f85436cfbe
Cillian Nolan <cillianolan@gmail.com>
2020-11-18 14:14:54 +0000

    added deck array

--- src/Blackjack/deck.java
+++ src/Blackjack/deck.java
@@ -1,4 +1,19 @@
 package Blackjack;
 
+import java.util.ArrayList;
+
 public class deck {
+    private ArrayList<card> cards;
+
+    public deck() {
+        this.cards = new ArrayList<card>();
+    }
+
+    public void fullDeck() {
+        for (cardType cardTypes : cardType.values()) {
+            for (cardValue cValue : cardValue.values()) {
+                this.cards.add(new card(cardTypes, cValue));
+            }
+        }
+    }
 }
