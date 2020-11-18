84bddeb641369dd0448b35dad469cf946087ed0a
Cillian Nolan <cillianolan@gmail.com>
2020-11-18 13:05:49 +0000

    card

--- src/Blackjack/card.java
+++ src/Blackjack/card.java
@@ -1,4 +1,22 @@
 package Blackjack;
 
 public class card {
+    private cardType cardTyp;
+    private cardValue cardVal;
+
+    public card(cardType card_Type,cardValue card_Value){
+        this.cardTyp = cardTyp;
+        this.cardVal = cardVal;
+    }
+
+    public cardValue getCard_Value(){
+        return this.cardVal;
+    }
+
+    @Override
+    public String toString(){
+        return
+                "\nCard Type: " + this.cardTyp.toString() + " \nValue: " + this.cardVal.toString();
+
+    }
 }
