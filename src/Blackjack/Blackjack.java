f0e3f137813d8e1963773e20d30b101202b12ea9
Cillian Nolan <cillianolan@gmail.com>
2020-11-30 16:28:37 +0000

    Removed YES_NO_Option due to difficulty. Instead switched to int 1,2. Also added return to deck to reset hand on each go.

--- src/Blackjack/Blackjack.java
+++ src/Blackjack/Blackjack.java
@@ -10,8 +10,7 @@ public class Blackjack {
 
         int bettingMoney = 250;
         int playerBet;
-
-
+        int ans;
 
 
         JOptionPane.showMessageDialog(null, "BlackJack Rules!\n\n1.      The goal of blackjack is to beat the dealer's hand without going over 21." +
@@ -35,62 +34,122 @@ public class Blackjack {
         deck playerHand = new deck();
         deck dealerHand = new deck();
 
-        while (bettingMoney>0){
+        while (bettingMoney > 0) {
 
-            playerBet = Integer.parseInt(JOptionPane.showInputDialog("Welcome to Nolan Casino Blackjack " + playerName  +
+            playerBet = Integer.parseInt(JOptionPane.showInputDialog("Welcome to Nolan Casino Blackjack " + playerName +
                     "\nYou have €" + bettingMoney + ", how much do you want to bet? Bet cannot exceed the money you have."));
 
-            if(bettingMoney<playerBet){JOptionPane.showMessageDialog(null, "You only have €" + bettingMoney +
-                    "! Please bet an amount lower or equal to this.");
+            if (bettingMoney < playerBet) {
+                JOptionPane.showMessageDialog(null, "You only have €" + bettingMoney +
+                        "! Please bet an amount lower or equal to this.");
+                System.exit(0);
 
             }
-            JOptionPane.showMessageDialog(null,"Dealer is shuffling deck.... get ready......");
-
+            JOptionPane.showMessageDialog(null, "Dealer is shuffling deck.... get ready......");
+            gameDeck.shuffle();
             playerHand.draw(gameDeck);
             playerHand.draw(gameDeck);
 
-            JOptionPane.showMessageDialog(null,"Your hand is: \n" + playerHand.toString() +
+            JOptionPane.showMessageDialog(null, "Your hand is: \n" + playerHand.toString() +
                     "\n\nYour hand is worth " + playerHand.cardVal());
 
             dealerHand.draw(gameDeck);
             dealerHand.draw(gameDeck);
-            JOptionPane.showMessageDialog(null,"Dealer's hand is: \n" + dealerHand.hitCard(1).toString() +
-                   "\nand\n" + "\n[FACEDOWN CARD]");
+            JOptionPane.showMessageDialog(null, "Dealer's hand is: \n" + dealerHand.hitCard(1).toString() +
+                    "\nand\n" + "\n[FACEDOWN CARD]");
+            if(playerHand.cardVal()>21){
+                JOptionPane.showMessageDialog(null,"BUST");
+            }
+            ans = Integer.parseInt(JOptionPane.showInputDialog(null,"Do you want to Hit or Stand?" +
+                    "\n\n1 = Hit" +
+                    "\n\n2 = Stand","Hit Or Stand?",JOptionPane.QUESTION_MESSAGE));
 
-            if (JOptionPane.showConfirmDialog(null, "Remember your hand is worth " + playerHand.cardVal() + "." +
-                            "\n\nWould you like to Hit or Stand?" + "\n\nYes for Hit, No for Stand.", "Hit or Stand?",
-                    JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION  ) {
+            if(ans == 1){
                 playerHand.draw(gameDeck);
-                JOptionPane.showMessageDialog(null,"You draw a " + playerHand.toString());
-                if(dealerHand.cardVal()<=17 || JOptionPane.YES_NO_OPTION == JOptionPane.YES_OPTION){
+                JOptionPane.showMessageDialog(null, "You draw a " + playerHand.toString());
+                if(playerHand.cardVal()>21){
+                    JOptionPane.showMessageDialog(null,"BUST");
+                    bettingMoney -= playerBet;
+                    playerHand.moveAllToDeck(gameDeck);
+                    dealerHand.moveAllToDeck(gameDeck);
+                    continue;
+
+                }
+                if (dealerHand.cardVal() > 17) {
+                    JOptionPane.showMessageDialog(null, "Dealer decided to not hit.");
+                    if(dealerHand.cardVal()>21){
+                        JOptionPane.showMessageDialog(null,"Dealer went bust! You win!");
+                        bettingMoney += playerBet;
+                        playerHand.moveAllToDeck(gameDeck);
+                        dealerHand.moveAllToDeck(gameDeck);
+                        continue;
+                    }
+                }
+                if (dealerHand.cardVal() < 17 || JOptionPane.YES_NO_OPTION == JOptionPane.YES_OPTION) {
                     dealerHand.draw(gameDeck);
-                    JOptionPane.showMessageDialog(null,"Dealer decided to Hit...." +
-                            "\n\nDealer hand is: " + dealerHand.toString());}
+                    JOptionPane.showMessageDialog(null, "Dealer decided to Hit....");
+                }
+
+                if (playerHand.cardVal() > dealerHand.cardVal()) {
+                    JOptionPane.showMessageDialog(null, "You win €" + playerBet +
+                            "\n\nYour new total balance is now €" + (bettingMoney + playerBet));
+                    bettingMoney += playerBet;
+                    while(playerBet == 0){
+                        JOptionPane.showMessageDialog(null,"You are broke!? You can't play anymore!!" +
+                                "\n\nNolan Blackjack Casino Security is now escorting you from the premises......" +
+                                "\n\nThe wife won't be happy you lost all the shillings.... Best of luck! Thanks for playing.");
+                    }
+                }
+                if (dealerHand.cardVal() > playerHand.cardVal()) {
+                    JOptionPane.showMessageDialog(null, "House wins! You lost €" + playerBet +
+                            "\n\nYour total balance is now €" + (bettingMoney - playerBet));
+                    bettingMoney -= playerBet;
+                    while(playerBet == 0){
+                        JOptionPane.showMessageDialog(null,"You are broke!? You can't play anymore!!" +
+                                "\n\nNolan Blackjack Casino Security is now escorting you from the premises......" +
+                                "\n\nThe wife won't be happy you lost all the shillings.... Best of luck! Thanks for playing.");
+                        System.exit(0);
+                    }
+                }
+
+
+                JOptionPane.showMessageDialog(null,"******Now Showing Player Hands******" +
+                        "\n\nPlayer Hand:\n " +playerHand.toString() +
+                        "\n\n\nDealer Hand:\n" +dealerHand.toString());
+            playerHand.moveAllToDeck(gameDeck);
+            dealerHand.moveAllToDeck(gameDeck);
+
             }
-            if(dealerHand.cardVal()>=17){
-                JOptionPane.showMessageDialog(null,"Dealer decided to not hit.");
+            if(ans==2){
+                if (playerHand.cardVal() > dealerHand.cardVal()) {
+                    JOptionPane.showMessageDialog(null, "You win €" + playerBet +
+                            "\n\nYour new total balance is now €" + (bettingMoney + playerBet));
+                    bettingMoney += playerBet;
+                    }
+                }
+                if (dealerHand.cardVal() > playerHand.cardVal()) {
+                    JOptionPane.showMessageDialog(null, "House wins! You lost €" + playerBet +
+                            "\n\nYour total balance is now €" + (bettingMoney - playerBet));
+                    bettingMoney -= playerBet;
+                    if(playerBet == 0)
+                        JOptionPane.showMessageDialog(null,"You are broke!? You can't play anymore!!" +
+                                "\n\nNolan Blackjack Casino Security is now escorting you from the premises......" +
+                                "\n\nThe wife won't be happy you lost all the shillings.... Best of luck! Thanks for playing.");
+                        System.exit(0);
+
             }
-            if(playerHand.cardVal()>dealerHand.cardVal()){
-                JOptionPane.showMessageDialog(null,"You win €" + playerBet +
-                        "Your new total balance is now €" + (bettingMoney+playerBet));
-                playerBet+=bettingMoney;
-            }
-            if(dealerHand.cardVal()>playerHand.cardVal()){
-                JOptionPane.showMessageDialog(null,"House wins! You lost €" + playerBet +
-                        "\n\nYour total balance is now €" + (bettingMoney-playerBet));
-                        bettingMoney -= playerBet;
-            }
+
+                playerHand.moveAllToDeck(gameDeck);
+                dealerHand.moveAllToDeck(gameDeck);
+
 
         }
-
-
-        }
-
-
 
 
 
     }
+}
+
 
 /* Referred to below sources for creating card classes.
  * http://math.hws.edu/javanotes/c5/s4.html
